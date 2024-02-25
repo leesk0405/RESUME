8cc9afe984ad1175add6860d666aa496f5793e8c
leesk0405 <leesk3102@gmail.com>
2024-02-25 19:56:12 +0900

    6th commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ $('header .nav_btn').click(function(){
 $('header nav .close_btn').click(function(){
   $('header').removeClass('active');
 })
+
+// $('.item .box').click(function(){
+//   $(this).parent('.item').toggleClass('active');
+//   $('.item .type').show();
+//   $(this).parent('.item').childern('.type').attr('data-aos', 'flip-up');
+// })
